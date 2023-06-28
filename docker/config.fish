df5c070381a9bfd42501078e6056e867fea5322f
kumanoryo <r-kumano@groovenauts.jp>
2023-06-28 18:20:48 +0900

    Add Dockerfile and copy file.

---
+++ docker/config.fish
@@ -0,0 +1,8 @@
+set -g theme_date_timezone UTC
+set -g theme_date_format "+%F %H:%M:%S (%Z)"
+set -g theme_color_scheme solarized
+set -g theme_newline_cursor yes
+
+set fish_color_command brpurple
+
+set -x CLOUDSDK_ACTIVE_CONFIG_NAME
