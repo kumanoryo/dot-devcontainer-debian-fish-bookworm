8bb8cba721b505c3dfbc4e80a53ca91c07c82d53
kumanoryo <r-kumano@groovenauts.jp>
2025-02-13 18:35:58 +0900

    単一の処理でarm64,amd64どちらにも対応できるようにした。

--- docker_arm64/config.fish
+++
@@ -1,8 +0,0 @@
-set -g theme_date_timezone UTC
-set -g theme_date_format "+%F %H:%M:%S (%Z)"
-set -g theme_color_scheme solarized
-set -g theme_newline_cursor yes
-
-set fish_color_command brpurple
-
-set -x CLOUDSDK_ACTIVE_CONFIG_NAME
